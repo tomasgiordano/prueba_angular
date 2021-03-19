334ebdab6d15128060a074e56c1d252b7d60d4f3
TomasGiordano <tomas.giordano@boreal-it.com>
2021-03-19 11:38:22 -0300

    asd

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const path = require('path');
 app.use(express.static(__dirname + '/prueba-angular'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/prueba-angular/index.html'));
+res.sendFile(path.join(__dirname + '/prueba-angular/app/app.component.html'));
 });
 console.log('Build successful!!');
